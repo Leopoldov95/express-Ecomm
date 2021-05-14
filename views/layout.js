5f2d646b2ed7769926fd29f4ad800c90b193fdd3
Leopoldov95 <leo386_ortega@mail.fresnostate.edu>
2021-05-13 19:52:54 -0500

    fixed title

--- views/layout.js
+++ views/layout.js
@@ -6,7 +6,7 @@ module.exports = ({ content }) => {
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Rydle's Cycles</title>
+    <title>Leo's Cycles</title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet"> 
     <link rel="stylesheet"  href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
