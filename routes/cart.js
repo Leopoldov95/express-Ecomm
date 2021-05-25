54050addd2ddfe13f0a052e1bba7a0ac190bd26c
Leopoldov95 <leo386_ortega@mail.fresnostate.edu>
2021-05-25 12:35:24 -0500

    debugging heroku app

--- routes/cart.js
+++ routes/cart.js
@@ -9,33 +9,37 @@ const {
 } = require("../controllers/main");
 
 router.post("/cart/products", async (req, res) => {
-  let cart; // create empty cart variable
+  try {
+    let cart; // create empty cart variable
 
-  if (!req.session.cartId) {
-    // we dont have a cart, we need to create one
-    // and store the cart id on the req.session.cartId property
-    cart = await addProduct({ items: [] }, "cart.json");
+    if (!req.session.cartId) {
+      // we dont have a cart, we need to create one
+      // and store the cart id on the req.session.cartId property
+      cart = await addProduct({ items: [] }, "cart.json");
 
-    req.session.cartId = cart.id;
-  } else {
-    // we have a cart, lets get it from the repository
-    cart = await getSingleItem(req.session.cartId, "cart.json");
+      req.session.cartId = cart.id;
+    } else {
+      // we have a cart, lets get it from the repository
+      cart = await getSingleItem(req.session.cartId, "cart.json");
+    }
+
+    const existingItem = cart.items.find(
+      (item) => item.id === req.body.productId
+    );
+
+    if (existingItem) {
+      // either increment quantity for existing product
+      existingItem.quantity++;
+    } else {
+      // OR add new item to existing array
+      cart.items.push({ id: req.body.productId, quantity: 1 });
+    }
+    await updateItem(cart.id, { items: cart.items }, "cart.json");
+
+    return res.redirect("/cart");
+  } catch (err) {
+    console.log(err);
   }
-
-  const existingItem = cart.items.find(
-    (item) => item.id === req.body.productId
-  );
-
-  if (existingItem) {
-    // either increment quantity for existing product
-    existingItem.quantity++;
-  } else {
-    // OR add new item to existing array
-    cart.items.push({ id: req.body.productId, quantity: 1 });
-  }
-  await updateItem(cart.id, { items: cart.items }, "cart.json");
-
-  return res.redirect("/cart");
 });
 
 router.get("/cart", async (req, res) => {
